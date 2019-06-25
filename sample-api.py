3fb3f450db3fd66cec9ae9bf806bb610c405aa0a
palakkeni5 <palakkeni@gmail.com>
2019-06-25 12:58:47 +0530

    customer order with db done

--- sample-api.py
+++ sample-api.py
@@ -4,11 +4,18 @@ from flask_cors import CORS
 from sample_model import load_model
 from new_model import call_from_api
 import json
+from flask_mysqldb import MySQL 
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 CORS(app)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'beproject'
+
+mysql = MySQL(app)
 
 books = [
     {'id': 0,
@@ -29,14 +36,44 @@ books = [
 ]
 
 
+@app.route('/db/save_orders',methods =['POST'])
+def index():
+    data=request.json
+    location=data["location"]
+    totalamount=data["totalamount"]
+    type=data["type"]
+    weight=data["weight"]
+    quantity=data["quantity"]
+    productName=data["productName"]
+    deliveryMode=data["deliveryMode"]
+
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO orders (location, totalamount, type, weight, quantity, productName, deliveryMode) VALUES ( %s , %s , %s, %s,%s,%s,%s)", (location,totalamount, type,weight,quantity,productName,deliveryMode))
+    mysql.connection.commit()
+    cur.close()
+    return jsonify('success')
+    # return request.data
+
+@app.route('/db/fetch_orders_all',methods=['GET'])
+def index1():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM orders")
+    row_headers=[x[0] for x in cur.description] #this will extract row headers
+    rv = cur.fetchall()
+    json_data=[]
+    for result in rv:
+        json_data.append(dict(zip(row_headers,result)))
+    return json.dumps(json_data)
+
+
 @app.route('/', methods=['GET'])
 def route():
     return jsonify(books)
 
-@app.route('/sample_model', methods= ['GET'])
-def bantai():
-    score=load_model()
-    return jsonify(score)
+# @app.route('/sample_model', methods= ['GET'])
+# def bantai():
+#     score=load_model()
+#     return jsonify(score)
 
 @app.route('/neural',methods= ['GET','POST'])
 def abc():
